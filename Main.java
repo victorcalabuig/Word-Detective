8c2af62eb5846d6446ab516cda1648b87211d9b8
Victor <victorcr1@icloud.com>
2019-06-29 18:48:47 +0200

    Add question to get the file name that the user wants to analyze and mechanism to read that file

--- Main.java
+++ Main.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		
+		
+		
+		//Welcome message
 		System.out.println("Welcome to Word-Detective 2.0");
+		
+		//Ask the user to choose a file and create a Scanner object to read that file.
+		System.out.println("Enter the file name you want to read:\n>>");
+		Scanner keyboard = new Scanner(System.in);
+		String fileName = keyboard.nextLine();
+		Scanner textReader = new Scanner(new File(fileName));
+		
+		
+		//Print the text of the file that the user chose
+		while(textReader.hasNextLine()) {
+			System.out.println(textReader.nextLine());
+		}
 
 	}
 
