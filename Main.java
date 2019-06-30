462cb41deb56983b92e639a1d37e33ca97d2c72e
Victor <victorcr1@icloud.com>
2019-06-30 10:43:16 +0200

    corrected the appearence of the first question made to the user

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main {
 		System.out.println("Welcome to Word-Detective 2.0");
 		
 		//Ask the user to choose a file and create a Scanner object to read that file.
-		System.out.println("Enter the file name you want to read:\n>>");
+		System.out.print("Enter the file name you want to read:\n>> ");
 		Scanner keyboard = new Scanner(System.in);
 		String fileName = keyboard.nextLine();
 		Scanner textReader = new Scanner(new File(fileName));
